e83b3e38e1ea4a08ce5256164124e025bc7f0f09
pravallika <aketypravallika@users.noreply.github.com>
2021-01-03 15:45:09 +0530

    main

--- src/app.js
+++ src/app.js
@@ -12,11 +12,23 @@ app.use(express.json());
 // Get all the students
 app.get('/students', async (req, res) => {
     // write your codes here
+    try{
+        const students = await Student.find();
+        res.status(200).json(students);
+    } catch(error) {
+        res.status(404).json({error})
+    }
+
 })
 
 // Add student to database
 app.post('/students', async (req, res) =>{
-    // write your codes here
+    try{
+        const student = await Student.create(req.body);
+        res.status(201).json(student);
+    }catch(error){
+        res.status(400).json({error})
+    }
 })
 
 // Get specific student
